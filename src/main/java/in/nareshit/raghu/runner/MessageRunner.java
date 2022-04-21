a05841cac51d3acde87f92c424e8199f5fd73844
sahoo <sahoo@RS-LAPTOP>
2022-04-21 18:37:42 +0530

    WH-996 COMMIT SECOND TIME

--- src/main/java/in/nareshit/raghu/runner/MessageRunner.java
+++ src/main/java/in/nareshit/raghu/runner/MessageRunner.java
@@ -8,9 +8,9 @@ public class MessageRunner implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception{
-		System.out.println("Welcome to Spring Boot App");
-		System.out.println("Welcome to app");
-		System.out.println("Welcome to model");
+		int a  =10;
+		System.out.println("Welcome to Spring Boot App" +a);
+		
 	}
 
 }
